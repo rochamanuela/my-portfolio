f82d8b5e5a9dfc85c7ff13cea5b4c2c9d4b0bf33
rochamanuela <rochadesouzamanuela@gmail.com>
2024-08-22 16:39:44 -0300

    style: continued responsiveness

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -12,17 +12,17 @@ export default function Experience() {
         <div className="child">
           <h1>Experiência</h1>
           <hr />
-          <p style={{ paddingLeft: '20%' }}>Desenvolvedora de Software e UI/UX Designer | Robert 
+          <p className='lide-experience-1'>Desenvolvedora de Software e UI/UX Designer | Robert 
             Bosch <br />Campinas, Brasil | Janeiro de 2023 - Presente</p>
         </div>
         <div className="child">
-          <p style={{ paddingRight: '14%' }}>Na Bosch foi onde tive e tenho a oportunidade de 
+          <p className='lide-experience-2'>Na Bosch foi onde tive e tenho a oportunidade de 
             aprender, evoluir e crescer na minha área. Durante este período participei de diversos 
             projetos, tais como:</p>
         </div>
       </div>
-      <section className="experience-content">
-        <div className="text" style={{ paddingLeft: '10%' }}>
+      <section className="experience-content" id="first-content">
+        <div className="text">
           <h1>BOSCH ESY - TCC 2024</h1>
           <p>Desenvolvi, junto com minha equipe, um software de gerenciamento de eventos
             corporativos. Neste projeto, atuei como UX/UI Designer, criando uma interface
@@ -40,7 +40,8 @@ export default function Experience() {
           <img src={MockupYoung} />
           {/*<div className="dark-purple-rectangle"></div>*/}
         </div>
-        <div className="text" style={{ paddingRight: '10%', paddingLeft: '5%' }}>
+        <div className="text" id="last-content">
+        {/* <div className="text" style={{ paddingRight: '10%', paddingLeft: '5%' }}> */}
           <h1>YOUNG BANK - Senai 2023</h1>
           <p>Como parte do projeto integrador do Senai, projetei a interface de um banco
             digital com um estilo moderno e voltado para o público jovem. O desenvolvimento
@@ -48,8 +49,9 @@ export default function Experience() {
             usuário fluida e acessível.</p>
         </div>
       </section>
-      <section className="experience-content">
-        <div className="text" style={{ paddingLeft: '10%', paddingRight: '5%' }}>
+      <section className="experience-content" id="last-content">
+        {/* <div className="text" style={{ paddingLeft: '10%', paddingRight: '5%' }}> */}
+        <div className="text">
           <h1>REX ROUTE - Rexroth 2023</h1>
           <p>Desenvolvi a interface de uma aplicação de compartilhamento de rota para a
             filial Rexroth, com o objetivo de diminuir custos. Trabalhei em estreita
